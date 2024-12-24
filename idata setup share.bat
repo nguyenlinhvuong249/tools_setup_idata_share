c71d643b471d495b0031d083ffd228c454fcaff8
Nguyễn Linh Vương <162272562+nguyenlinhvuong249@users.noreply.github.com>
2024-12-24 10:56:40 +0700

    Update idata setup share.bat

--- idata setup share.bat
+++ idata setup share.bat
@@ -6,9 +6,10 @@
     ADB SETUP IDATA by minitools (Minimal ADB and Fastboot)
     adb start-server
     rem bạn hãy thay đổi và pass wifi tại đây ================================================================================================
-                set duong_dan_app_ban_cu=D:\ANT\Program\File_APP_APK\JTSprinter_version\JTSprinter1.1.125.apk
+                ::set duong_dan_app_ban_cu=D:\ANT\Program\File_APP_APK\JTSprinter_version\JTSprinter1.1.125.apk
                 ::set duong_dan_app_ban_moi=D:\ANT\Program\File_APP_APK\JTSprinter_version\JTSprinter1.1.151.apk
 		set duong_dan_app_ban_moi=%1
+		set duong_dan_app_ban_cu=%2
                 set packages_name_JT_cu_1=cn.yssoft.vietnam
                 set packages_name_JT_cu_2=cn.yssoft.philippines
                 set packages_name_JT_moi=com.jt.express.vietnam.outfield
@@ -40,6 +41,7 @@
         )
             :no_device
 		echo %duong_dan_app_ban_moi%
+		echo %duong_dan_app_ban_cu%
                 echo.
                 echo.
                 echo ========================================= Xac nhan thuc hien ==========================================
