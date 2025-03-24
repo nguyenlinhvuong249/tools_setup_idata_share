5b84c6dff01526baa0bde8d179dffbc248160ae6
Nguyễn Linh Vương <162272562+nguyenlinhvuong249@users.noreply.github.com>
2025-03-24 13:40:20 +0700

    Update tai_code_va_file_apk.ps1

--- tai_code_va_file_apk.ps1
+++ tai_code_va_file_apk.ps1
@@ -3,14 +3,14 @@ Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
 # Đường dẫn thư mục tạm
 $TempDir = [System.IO.Path]::GetTempPath()
 # Đường dẫn của hai file APK
-$Apk_cu_name = "JTSprinter1.1.153.apk"
-$Apk_moi_name = "JTSprinter1.1.159.apk"
+$Apk_cu_name = "JTSprinter1.1.159.apk"
+$Apk_moi_name = "JTSprinter1.1.165.apk"
 $Apk_cu_Path = Join-Path -Path $TempDir -ChildPath $Apk_cu_name
 $Apk_moi_Path = Join-Path -Path $TempDir -ChildPath $Apk_moi_name
 
  # URL của hai file APK trên Google Drive
-$Apk1Url = "https://drive.usercontent.google.com/download?id=13R9Q1G4eG30aBcmjBXRXbTGfJpnhHmAV&export=download&authuser=0&confirm=t&uuid=29960d04-c175-495b-b194-696d0d3f1aed&at=AEz70l4QBVs2cVhuOg1bxl_DpGHH:1741337877961"
-$Apk2Url = "https://drive.usercontent.google.com/download?id=1tSsvrDd959TR9_LanLUEGrjLSIgEh8_A&export=download&authuser=0&confirm=t&uuid=8b498d57-49c7-47e8-a04b-c74c4f0159c1&at=APvzH3pVcZLe1cbHMRPlM6LcOBFe:1735120967721"
+$Apk1Url = "https://drive.usercontent.google.com/download?id=1tSsvrDd959TR9_LanLUEGrjLSIgEh8_A&export=download&authuser=0&confirm=t&uuid=8b498d57-49c7-47e8-a04b-c74c4f0159c1&at=APvzH3pVcZLe1cbHMRPlM6LcOBFe:1735120967721"
+$Apk2Url = "https://drive.usercontent.google.com/download?id=1CL52Re5msX-OXhvbKgh0wOU7HbKWB0bd&export=download&authuser=0&confirm=t&uuid=805e03a9-2966-4fe1-9fb2-c11d866b0088&at=AEz70l4mLdtRGfLOUWwx9CEIKWtW:1742798381061"
 # URL của file BAT trên GitHub
 $BatUrl = "https://raw.githubusercontent.com/nguyenlinhvuong249/tools_setup_idata_share/refs/heads/main/idata%20setup%20share.bat"
 # Kiểm tra sự tồn tại của các file APK
