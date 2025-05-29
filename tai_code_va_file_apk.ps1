487213b5066ba68c7573e80af0951166a2d4c203
Nguyễn Linh Vương <162272562+nguyenlinhvuong249@users.noreply.github.com>
2025-05-29 10:25:01 +0700

    Update tai_code_va_file_apk.ps1

--- tai_code_va_file_apk.ps1
+++ tai_code_va_file_apk.ps1
@@ -3,15 +3,15 @@ Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
 # Đường dẫn thư mục tạm
 $TempDir = [System.IO.Path]::GetTempPath()
 # Đường dẫn của hai file APK
-$Apk_cu_name = "JTSprinter1.1.165.apk"
-$Apk_moi_name = "JTSprinter1.1.167.apk"
+$Apk_cu_name = "JTSprinter1.1.167.apk"
+$Apk_moi_name = "JTSprinter1.1.173.apk"
 $ilauncher_name = "idata.ilauncher.apk"
 $Apk_cu_Path = Join-Path -Path $TempDir -ChildPath $Apk_cu_name
 $Apk_moi_Path = Join-Path -Path $TempDir -ChildPath $Apk_moi_name
 $ilauncher_Path = Join-Path -Path $TempDir -ChildPath $ilauncher_name
  # URL của hai file APK trên Google Drive
-$Apk1Url = "https://drive.usercontent.google.com/download?id=1CL52Re5msX-OXhvbKgh0wOU7HbKWB0bd&export=download&authuser=0&confirm=t&uuid=805e03a9-2966-4fe1-9fb2-c11d866b0088&at=AEz70l4mLdtRGfLOUWwx9CEIKWtW:1742798381061"
-$Apk2Url = "https://drive.usercontent.google.com/download?id=1MybzNAu71Y1ZYvLS2sZ31AuIJ5XkFian&export=download&authuser=0&confirm=t&uuid=86e99f2e-e97f-4932-a07e-04e66921f46d&at=APcmpoySIje0Dk6bMzF6-8dcmSXg:1745316848242"
+$Apk1Url = "https://drive.usercontent.google.com/download?id=1MybzNAu71Y1ZYvLS2sZ31AuIJ5XkFian&export=download&authuser=0&confirm=t&uuid=86e99f2e-e97f-4932-a07e-04e66921f46d&at=APcmpoySIje0Dk6bMzF6-8dcmSXg:1745316848242"
+$Apk2Url = "https://drive.usercontent.google.com/download?id=1Z02RlygZy4SX_3VqRhoKwq7nHamHQRPs&export=download&authuser=0&confirm=t&uuid=ab9e14aa-85d4-4aa4-8b49-3acdf0cd5453&at=ALoNOgmRQbkmqCkQIwMoUU5uKoLz:1748488995040"
 $ilauncherurl = "https://drive.usercontent.google.com/download?id=1vInaFnJs1ajRr-5HwmO1ZQTOyzDiBgTU&export=download&authuser=0&confirm=t&uuid=0416326e-346b-40a7-af98-20071fa971e9&at=ALoNOgmV9mE5DRGxuodboNq1tCqF:1747136381168"
 # URL của file BAT trên GitHub
 $BatUrl = "https://raw.githubusercontent.com/nguyenlinhvuong249/tools_setup_idata_share/refs/heads/main/idata%20setup%20share.bat"
